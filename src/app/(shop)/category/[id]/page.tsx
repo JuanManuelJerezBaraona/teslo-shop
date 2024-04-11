29d71636ec26a80ecdc483085a6fba0803331a9f
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-04-11 07:52:21 -0400

    Refactor category page to handle 'kids' id and add Title component to shop page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,4 +1,8 @@
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+
+const seedProducts = initialData.products;
 
 interface Props {
     params: {
@@ -9,15 +13,24 @@ interface Props {
 export default function({ params }: Props) {
 
     const { id } = params;
+    const products = seedProducts.filter( product => product.gender === id );
 
-    if ( id === 'kids' ) {
-        notFound();
+    // if ( id === 'kids' ) {
+    //     notFound();
         
-    }
+    // }
 
     return (
-        <div>
-            <h1>Category Page { id }</h1>
-        </div>
+        <>
+            <Title 
+                title="Artículos de"
+                subtitle="Todos los productos"
+                className="mb-2"
+            />
+
+            <ProductGrid 
+                products={ products } 
+            />
+        </>
     );
 }
