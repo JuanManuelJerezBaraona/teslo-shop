8593011d30cad0c62f347af3cf50cae0f9b52191
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-04-15 10:46:33 -0400

    Add seed script to package.json and update README.md

--- README.md
+++ README.md
@@ -5,9 +5,10 @@
 1. Clonar el repositorio
 2. Crear una copia del archivo ```.env.template``` y renombrarlo a ```.env``` y cambiar las variables de entorno
 3. Instalar dependencias ```npm install```
-4. Levantar la base de datos con docker ```docker compose up -d```
+4. Abrir Docker y levantar la base de datos con docker ```docker compose up -d```
 5. Correr las migraciones de Prisma ```npx prisma migrate dev```
-6. Correr el proyecto ```npm run dev```
+6. Ejecutar seed ```npm run seed```
+7. Correr el proyecto ```npm run dev```
 
 
 
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,62 @@
+import { initialData } from "./seed";
+import prisma from '../lib/prisma';
+
+async function main() {
+
+    // 1. Borrar registro previos
+    await prisma.productImage.deleteMany();
+    await prisma.product.deleteMany();
+    await prisma.category.deleteMany();
+
+    // 2. Crear Categorías
+    const { categories, products } = initialData;
+
+    const categoriesData = categories.map(category => ({
+        name: category
+    }));
+
+    await prisma.category.createMany({
+        data: categoriesData
+    });
+
+    const categoriesDB = await prisma.category.findMany();
+
+    const categoriesMap = categoriesDB.reduce((map, category) => {
+        map[category.name.toLowerCase()] = category.id;
+        return map;
+    }, {} as Record<string, string>); // <string=shirt, string=categoryID>
+
+    // 3. Crear Productos
+    products.forEach(async (product) => {
+
+        const { type, images, ...rest } = product;
+
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryId: categoriesMap[type]
+            }
+        })
+
+        // 4. Crear Imágenes
+        const imagesData = images.map(image => ({
+            url: image,
+            productId: dbProduct.id
+        }));
+
+        await prisma.productImage.createMany({
+            data: imagesData
+        });
+
+    });
+
+    console.log('Seed executed correctly');
+}
+
+// Función anónima autoinvocada
+(() => {
+
+    if (process.env.NODE_ENV === 'production') return;
+
+    main();
+})();
