ca6016632ea5117a431b78581887cab5e243dcd9
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-04-16 13:17:58 -0400

    Refactor page component names in the shop directory

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,11 @@
+export const revalidate = 604800; // 7 días aprox.
+import { Metadata, ResolvingMetadata } from "next";
+
 import { notFound } from "next/navigation";
 
-import { initialData } from "@/seed/seed";
 import { titleFont } from "@/config/fonts";
-import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
+import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector, StockLabel } from "@/components";
+import { getProductBySlug } from "@/actions";
 
 interface Props {
     params: {
@@ -10,53 +13,81 @@ interface Props {
     }
 }
 
-export default function ProductBySlugPage({ params }: Props) {
+export async function generateMetadata(
+    { params }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    // read route params
+    const slug = params.slug;
+
+    // fetch data
+    const product = await getProductBySlug(slug);
+
+    // optionally access and extend (rather than replace) parent metadata
+    // const previousImages = (await parent).openGraph?.images || []
+
+    return {
+        title: product?.title ?? 'Producto no encontrado',
+        description: product?.description ?? '',
+        openGraph: {
+            title: product?.title ?? 'Producto no encontrado',
+            description: product?.description ?? '',
+            images: [`/products/${ product?.images[1] }`],
+        },
+    }
+}
+
+export default async function ProductBySlugPage({ params }: Props) {
 
     const { slug } = params;
-    const product = initialData.products.find( product => product.slug === slug );
+    const product = await getProductBySlug(slug);
 
-    if ( !product ) {
+    if (!product) {
         notFound();
     }
 
     return (
         <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
-            
+
             {/* Slide show */}
             <div className="col-span-1 md:col-span-2">
 
                 {/* Mobile Slideshow */}
-                <ProductMobileSlideshow 
-                    title={ product.title }
-                    images={ product.images }
+                <ProductMobileSlideshow
+                    title={product.title}
+                    images={product.images}
                     className="block md:hidden"
                 />
 
 
                 {/* Desktop Slideshow */}
-                <ProductSlideshow 
-                    title={ product.title }
-                    images={ product.images }
+                <ProductSlideshow
+                    title={product.title}
+                    images={product.images}
                     className="hidden md:block"
                 />
             </div>
 
             {/* Detalles */}
             <div className="col-span-1 px-5">
-                <h1 className={ `${ titleFont.className } antialiased font-bold text-xl` }>
-                    { product.title }
+
+                <StockLabel slug={product.slug} />
+
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+                    {product.title}
                 </h1>
-                <p className="text-lg mb-5">${ product.price }</p>
+
+                <p className="text-lg mb-5">${product.price}</p>
 
                 {/* Selector de Tallas */}
-                <SizeSelector 
-                    selectedSize={ product.sizes[1] }
-                    availableSizes={ product.sizes }
+                <SizeSelector
+                    selectedSize={product.sizes[1]}
+                    availableSizes={product.sizes}
                 />
 
                 {/* Selector de cantidad */}
-                <QuantitySelector 
-                    quantity={ 1 }
+                <QuantitySelector
+                    quantity={1}
                 />
 
                 {/* Button */}
@@ -67,7 +98,7 @@ export default function ProductBySlugPage({ params }: Props) {
                 {/* Descripción */}
                 <h3 className="font-bold text-sm">Descripción</h3>
                 <p className="font-light">
-                    { product.description }
+                    {product.description}
                 </p>
 
             </div>
